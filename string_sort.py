3c340c5347767bef9650bc3ddbecadecb3784e1b
Eric Daily <dailye@oregonstate.edu>
2022-07-09 14:48:33 -0400

    Initial commit

---
+++ string_sort.py
@@ -0,0 +1,10 @@
+
+def insertion_sort(word_list):
+    for i in range(1, len(word_list)):
+        j = i
+        while word_list[j - 1].lower() > word_list[j].lower() and j > 0:
+            temp = word_list[j - 1]
+            word_list[j - 1] = word_list[j]
+            word_list[j] = temp
+            j -= 1
+
