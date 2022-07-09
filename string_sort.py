5daec05ba8a1b5b434d9e5e0a06694a3ba7bcb4a
Eric Daily <dailye@oregonstate.edu>
2022-07-09 14:50:00 -0400

    yea

--- string_sort.py
+++ string_sort.py
@@ -1,5 +1,5 @@
 
-def insertion_sort(word_list):
+def string_sort(word_list):
     for i in range(1, len(word_list)):
         j = i
         while word_list[j - 1].lower() > word_list[j].lower() and j > 0:
